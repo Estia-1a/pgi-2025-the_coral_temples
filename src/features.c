a130b7565504cecab41b1863d42d74376b1298cc
Matthias380 <matthias.aromatario@etu.estia.fr>
2025-05-26 09:25:49 +0200

    Réglage conflit

--- src/features.c
+++ src/features.c
@@ -14,3 +14,18 @@
 void helloWorld() {
     printf("Hello World !");
 }
+
+void first_pixel(char *source_path) {
+    unsigned char *data;
+    int width, height, channels;
+
+    read_image_data(source_path, &data, &width, &height, &channels);
+
+    int r, g, b;
+    r = data[0];
+    /*int g = data[3][1];
+    int b = data[3][2];
+
+    printf("first_pixel: %d, %d, %d\n",r, g, b);*/
+
+}
