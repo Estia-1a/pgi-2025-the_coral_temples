170df9ec53ac09da13e6ade101bab9c43360841b
Matthias380 <matthias.aromatario@etu.estia.fr>
2025-05-26 09:53:04 +0200

    reglage

--- src/features.c
+++ src/features.c
@@ -17,15 +17,13 @@ void helloWorld() {
 
 void first_pixel(char *source_path) {
     unsigned char *data;
-    int width, height, channels;
-
-    read_image_data(source_path, &data, &width, &height, &channels);
-
+    int width, height, channel_count;
+    read_image_data(source_path, &data, &width, &height, &channel_count);
     int r, g, b;
     r = data[0];
-    /*int g = data[3][1];
-    int b = data[3][2];
+    g = data[1];
+    b = data[2];
 
-    printf("first_pixel: %d, %d, %d\n",r, g, b);*/
+    printf("first_pixel: %d, %d, %d\n",r, g, b);
 
 }
