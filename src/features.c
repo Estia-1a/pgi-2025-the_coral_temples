762461d31cbf964ed9f844b5144db1fdce393bb1
LouisFoucher <louis.foucher--jube@etu.estia.fr>
2025-06-13 09:58:43 +0200

    Dernière version tenth_pixel

--- src/features.c
+++ src/features.c
@@ -1,9 +1,8 @@
 #include <estia-image.h>
 #include <stdio.h>
-#include <string.h>
+
 #include "features.h"
 #include "utils.h"
-#include <stdlib.h>
 
 
 /**
@@ -17,22 +16,18 @@ void helloWorld() {
     printf("Hello World !");
 }
 
-void tenth_pixel(char *source_path) {
-    unsigned char *data;
-    int width, height, channels;
-
-    read_image_data(source_path, &data, &width, &height, &channels);
-
-    int pixel_place = 9;
-    int i = pixel_place * channels;
-    printf("tenth_pixel: %d, %d, %d\n", data[i], data[i + 1], data[i + 2]);
-
-    data[i]     = 255;
-    data[i + 1] = 0;
-    data[i + 2] = 0;
-
-    write_image_data(source_path, data, width, height);
-
-    free(data);
+void tenth_pixel(char *source_path){
+    unsigned char *data = NULL;
+    int width, height, channel_count;
+    
+    read_image_data(source_path, &data, &width, &height, &channel_count);
+ 
+    int r, g, b;
+    r = data[27];
+    g = data[28];
+    b = data[29];
+ 
+    printf("tenth_pixel: %d, %d, %d\n",r, g, b);
+ 
 }
 
